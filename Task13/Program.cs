fe831b15c766383541f7cd67e6febef3be7e5d8f
Valeria K <valeriafo@mail.ru>
2022-05-30 20:20:28 +0500

    done Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int num = new Random().Next();
+Console.WriteLine(num);
+if (num >99)
+{
+string text = num.ToString();
+Console.WriteLine("Третья цифра заданного числа = " + text [2]);}
+else 
+Console.WriteLine("Третьей цифры нет");
